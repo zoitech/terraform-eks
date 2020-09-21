658654aee90f59c8753a797abba6d2d7f3f1471f
ZOI\oliver.annau <22314584+oliver1980@users.noreply.github.com>
2020-09-21 15:24:32 +0200

    updating variables def w/ defaults so module call stays tidy

--- variables.tf
+++ variables.tf
@@ -1,18 +1,38 @@
 variable "region" {}
 variable "cluster-name" {}
-variable "eks-version" {}
-variable "nodes-count" {}
-variable "max-nodes-count" {}
-variable "min-nodes-count" {}
-variable "instance-types" {}
+variable "eks-version" {
+  default = "1.17"
+}
+variable "nodes-count" {
+  default = "1"
+}
+variable "max-nodes-count" {
+  default = "5"
+}
+variable "min-nodes-count" {
+  default = "1"
+}
+variable "instance-types" {
+  default = "t3.micro" # smallest possible as default
+}
 variable "vpc-id" {}
 variable "cluster-subnet-ids" {}
 variable "eks-additional-security-groups" {
   default = []
 }
 variable "nodes-subnets-ids" {}
-variable "enable-autoscaler-iam" {}
-variable "nodes-version" {}
-variable "nodes-ami-release" {}
-variable "enable-private-access" {}
-variable "enable-public-access" {}
+variable "enable-autoscaler-iam" {
+  default = true
+}
+variable "nodes-version" {
+  default = "1.17"
+}
+variable "nodes-ami-release" {
+  default = "1.17.9-20200904"
+}
+variable "enable-private-access" {
+  default = true
+}
+variable "enable-public-access" {
+  default = false
+}
