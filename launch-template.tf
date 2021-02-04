39ef6c06d54c3e4a96bcb762540ba7b76495553c
Pedro Machado <pedro.machado@zoi.de>
2021-02-04 19:24:07 +0000

    Support for Node Group tags, ammended a typo.

--- launch-template.tf
+++ launch-template.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "cluster-nodes-launch-template" {
   
   instance_type = var.instance-types
   image_id = var.nodes-ami-release
-  #vpc_security_group_ids = aws_security_group.group-eks-nodes.id
+  vpc_security_group_ids = aws_security_group.group-eks-nodes.id
 
   tag_specifications {
     resource_type = "instance"
