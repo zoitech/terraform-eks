b05c91f67d4fcac57c7c7b5b78169f5b8b4db0ac
Pedro Machado <pedro.machado@zoi.de>
2021-02-12 10:10:35 +0000

    change sg in lt to flatten

--- launch-template.tf
+++ launch-template.tf
@@ -2,7 +2,7 @@ resource "aws_launch_template" "cluster-nodes-launch-template" {
   name = "cluster-nodes-launch-template"
   
   instance_type = var.instance-types
-  vpc_security_group_ids = concat([aws_security_group.Group-eks-nodes.id, aws_security_group.Group-eks.id], var.nodes-additional-security-groups)
+  vpc_security_group_ids = flatten([aws_security_group.Group-eks-nodes.id, aws_security_group.Group-eks.id], var.nodes-additional-security-groups)
   
 
   tag_specifications {
