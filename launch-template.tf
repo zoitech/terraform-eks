d62e2059a0b5975467af8c5cf15389b6b9a5dc5a
Pedro Machado <pedro.machado@zoi.de>
2021-03-01 20:46:20 +0000

    Update launch-template.tf

--- launch-template.tf
+++ launch-template.tf
@@ -2,7 +2,7 @@ resource "aws_launch_template" "cluster-nodes-launch-template" {
   name = "cluster-nodes-launch-template"
   
   instance_type = var.instance-types
-  vpc_security_group_ids = flatten([[aws_security_group.Group-eks-nodes.id, aws_security_group.Group-eks.id], var.nodes-additional-security-groups])
+  vpc_security_group_ids = flatten([[aws_security_group.Group-eks-nodes.id, aws_security_group.Group-eks.id, aws_eks_cluster.cluster-masters[*].vpc_config[0].cluster_security_group_id], var.nodes-additional-security-groups])
   
 
   tag_specifications {
