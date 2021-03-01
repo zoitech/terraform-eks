b78af8d0160a5e2e506302c639c80fc2f9836a22
Pedro Machado <pedro.machado@zoi.de>
2021-03-01 18:15:52 +0000

    Update cluster-nodes.tf, outputs.tf files

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "oidc_provider_issuer" {
 output "cluster-arn" {
   value = aws_eks_cluster.cluster-masters.arn
 }
+
+output "cluster_primary_security_group_id" {
+  description = "The cluster primary security group ID created by the EKS cluster on 1.14 or later. Referred to as 'Cluster security group' in the EKS console."
+  value       = local.cluster_primary_security_group_id
+}
