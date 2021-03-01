ecafd395488a6c99a86f2e041452e692f713baa7
Pedro Machado <pedro.machado@zoi.de>
2021-03-01 18:25:51 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,5 +8,5 @@ output "cluster-arn" {
 
 output "cluster_primary_security_group_id" {
   description = "The cluster primary security group ID created by the EKS cluster on 1.14 or later. Referred to as 'Cluster security group' in the EKS console."
-  value       = local.cluster_primary_security_group_id
+  value       = element(concat(aws_eks_cluster.cluster-masters[*].vpc_config[0].cluster_security_group_id, list("")), 0)
 }
