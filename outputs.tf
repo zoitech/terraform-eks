01233950f44d4400f29ad43476111001a92e7111
Pedro Machado <pedro.machado@zoi.de>
2021-04-28 10:03:45 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,8 @@ output "cluster_primary_security_group_id" {
   description = "The cluster primary security group ID created by the EKS cluster on 1.14 or later. Referred to as 'Cluster security group' in the EKS console."
   value       = element(concat(aws_eks_cluster.cluster-masters[*].vpc_config[0].cluster_security_group_id, list("")), 0)
 }
+
+output "eks_nodes_role" {
+  description = "IAM role used by EKS node group."
+  value       = aws_iam_role.nodes.arn
+}
