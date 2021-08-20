46296680273193562dc4bac268b2134374222b60
hugo.rodrigues <hugo.rodrigues@zoi.de>
2021-08-20 16:31:43 +0100

    Add dependencies to the resources

--- cluster-addon.tf
+++ cluster-addon.tf
@@ -3,19 +3,23 @@ resource "aws_eks_addon" "kube_proxy" {
 
     cluster_name = var.cluster-name
     addon_name = "kube-proxy"
-    #addon_version = var.eks_addon_version_kube_proxy
     resolve_conflicts = "OVERWRITE"
-  
-}
+    
+    depends_on = [
+      aws_eks_cluster.cluster-nodes
+    ]
 
+}
 resource "aws_eks_addon" "core_dns" {
     count = var.create_eks_addons ? 1 : 0 
 
     cluster_name = var.cluster-name
     addon_name = "coredns"
-    #addon_version = var.eks_addon_version_core_dns
     resolve_conflicts = "OVERWRITE"
-  
+
+    depends_on = [
+      aws_eks_cluster.cluster-nodes
+    ]
 }
 
 resource "aws_eks_addon" "vpc-cni" {
@@ -24,4 +28,8 @@ resource "aws_eks_addon" "vpc-cni" {
     cluster_name = var.cluster-name
     addon_name = "vpc-cni"
     resolve_conflicts = "OVERWRITE"
+
+    depends_on = [
+      aws_eks_cluster.cluster-nodes
+    ]
 }
